5205374448e355e4cdbb5171f4755812db2ddc14
Sergey Kasatkin (skasatki) <skasatki@cisco.com>
2019-03-25 14:52:23 -0700

    Added unique character checker in a string

--- src/strings_and_arrays/isUnique.java
+++ src/strings_and_arrays/isUnique.java
@@ -1,9 +1,32 @@
 package strings_and_arrays;
 
+//Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+
 public class isUnique {
 
 	public isUnique() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	private final static int ASCII_LIMIT = 128;
+	private static boolean isUniqueString1(String str) throws Exception {
+		if (str == null || str.isEmpty()) {
+			throw new Exception ("Empty string");
+		}
+		boolean[] arr = new boolean[ASCII_LIMIT];
+		for (int i = 0; i < str.length(); i++) {
+			int shift = str.charAt(i) - 'a';
+			if (!arr[shift]) {
+				arr[shift] = true;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println(isUniqueString1("aa"));
 	}
 
 }
